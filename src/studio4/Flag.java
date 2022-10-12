261281926811461b868770d3b4a17f45da12735c
ChloeSampere <ChloeSampere@macbook-air-10.dhcp.wustl.edu>
2022-10-12 15:42:20 -0500

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,19 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.rectangle(0.5, 0.5, 0.25, 0.15);
 		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(.5, .6, .25, .05);
+		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(.5, .4, .25, .05);
+		
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledRectangle(.625, .5, .125, .05);
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledCircle(.375, .5, .02);
 	}
 }
